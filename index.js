4319e45a4dbfd25ea5f77339e9546b9222471754
Asarua <1299458195@qq.com>
2021-03-10 15:59:23 +0800

    测试

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const fs = require('fs')
 const msgPath = process.env.GIT_PARAMS
+const chalk = require('chalk')
+const msg = fs.readFileSync(msgPath, 'utf8').trim()
 
-console.log(msgPath, 22222)
+if (!/测试/.test(msg)) {
+  console.error(
+    chalk.red('提交的时候commit msg不是测试！')
+  )
+  process.exit(1)
+}
+
